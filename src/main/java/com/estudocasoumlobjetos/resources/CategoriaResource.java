8613130edc82bcff135c53664790ea7e092b3244
Clayson Caitano da Silva <clayson.caitano@gmail.com>
2023-05-21 15:10:34 -0300

    Classe Categoria

--- src/main/java/com/estudocasoumlobjetos/resources/CategoriaResource.java
+++ src/main/java/com/estudocasoumlobjetos/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.estudocasoumlobjetos.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.estudocasoumlobjetos.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Roupa");
+		Categoria cat2 = new Categoria(2, "Calçado");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		return categorias;
 	}
 }
