9fe30dc5e9f001a76b42c6149a8b84688b4e64a3
Clayson Caitano da Silva <clayson.caitano@gmail.com>
2023-05-21 13:01:51 -0300

    Projeto criado

---
+++ src/test/java/com/estudocasoumlobjetos/EstudocasoumlobjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudocasoumlobjetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudocasoumlobjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
