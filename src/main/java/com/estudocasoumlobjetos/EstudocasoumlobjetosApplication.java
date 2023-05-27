247d5b56193443969123ba08af6d706a0f48b006
Clayson Caitano da Silva <clayson.caitano@gmail.com>
2023-05-27 19:19:40 -0300

    Estados e Cidades

--- src/main/java/com/estudocasoumlobjetos/EstudocasoumlobjetosApplication.java
+++ src/main/java/com/estudocasoumlobjetos/EstudocasoumlobjetosApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.estudocasoumlobjetos.domain.Categoria;
+import com.estudocasoumlobjetos.domain.Cidade;
+import com.estudocasoumlobjetos.domain.Estado;
 import com.estudocasoumlobjetos.domain.Produto;
 import com.estudocasoumlobjetos.repositories.CategoriaRepository;
+import com.estudocasoumlobjetos.repositories.CidadeRepository;
+import com.estudocasoumlobjetos.repositories.EstadoRepository;
 import com.estudocasoumlobjetos.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class EstudocasoumlobjetosApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudocasoumlobjetosApplication.class, args);
@@ -31,10 +41,19 @@ public class EstudocasoumlobjetosApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
-		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		Estado e1 = new Estado(null, "São Paulo");
+		Estado e2 = new Estado(null, "Minas Gerais");
+		
+		Cidade c1 = new Cidade(null, "São Paulo", e1);
+		Cidade c2 = new Cidade(null, "Campinas", e1);
+		Cidade c3 = new Cidade(null, "Uberlândia", e2);
+		
+		e1.getCidades().addAll(Arrays.asList(c1, c2));
+		e1.getCidades().addAll(Arrays.asList(c3));
 		
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
@@ -42,9 +61,12 @@ public class EstudocasoumlobjetosApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
+	
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		estadoRepository.saveAll(Arrays.asList(e1, e2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
