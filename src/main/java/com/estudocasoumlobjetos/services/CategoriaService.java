91945f7620b855e94f55bace13ee293069568a2e
Clayson Caitano da Silva <clayson.caitano@gmail.com>
2023-05-27 18:42:37 -0300

    Tratativa de erros

--- src/main/java/com/estudocasoumlobjetos/services/CategoriaService.java
+++ src/main/java/com/estudocasoumlobjetos/services/CategoriaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.estudocasoumlobjetos.domain.Categoria;
+import com.estudocasoumlobjetos.exceptions.ObjectNotFoundException;
 import com.estudocasoumlobjetos.repositories.CategoriaRepository;
 
 @Service
@@ -16,6 +17,6 @@ public class CategoriaService {
 	
 	public Categoria Buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado para o Id = " + id + ", tipo = " + Categoria.class.getName()));
 	}
 }
